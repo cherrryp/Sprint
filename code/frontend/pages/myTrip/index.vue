774189349182ac29f29dc38b359e6741cc471e3a
Kimmypim <pimapsorn81047@gmail.com>
2026-03-16 21:24:41 +0700

    เพิ่มรีวิวหน้า ค้นหา

--- code/frontend/pages/myTrip/index.vue
+++ code/frontend/pages/myTrip/index.vue
@@ -250,7 +250,7 @@
                         <div
                           v-for="review in getDriverReviewsPage(trip.driverId)"
                           :key="review.id"
-                          class="p-3 border border-gray-300 rounded-md bg-white"
+                          class="p-3 border border-gray-300 rounded-md bg-white hover:bg-gray-50"
                         >
                           <!-- Reviewer info row -->
                           <div class="flex items-center gap-2 mb-2">
